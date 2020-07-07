59a171386803ae2f36e0d3446a07022454110bfb
vladimir_km8 <vladimirkm8@gmail.com>
2020-07-07 17:06:21 +0300

    Test

--- main.c
+++ main.c
@@ -8,12 +8,15 @@ int main(void)
                   __builtin_sysreg_write(__FLAGREGST, 0x01); // зажигаем светодиод FLAG0
                   Delay (500000);
                   __builtin_sysreg_write(__FLAGREGCL, 0x00); // гасим светодиод FLAG0
+                  
                   __builtin_sysreg_write(__FLAGREGST, 0x02); // зажигаем светодиод FLAG1
                   Delay (500000);
                   __builtin_sysreg_write(__FLAGREGCL, 0x00);
+                  
                   __builtin_sysreg_write(__FLAGREGST, 0x04); // зажигаем светодиод FLAG2
                   Delay (500000);
                   __builtin_sysreg_write(__FLAGREGCL, 0x00);
+                  
                   __builtin_sysreg_write(__FLAGREGST, 0x08); // зажигаем светодиод FLAG3
                   Delay (500000);
                   __builtin_sysreg_write(__FLAGREGCL, 0x00);
