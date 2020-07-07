3c97195806c425ab423e940f1afe37fdc27f9e9c
Vladimirkm8 <44296912+Vladimirkm8@users.noreply.github.com>
2020-07-07 16:51:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,20 +3,20 @@
 void Delay(int waitTicks);
 int main(void)
 {
-HAL_SYS_FlagEnable(); // Основной цикл
-while(1) {
-__builtin_sysreg_write(__FLAGREGST, 0x01); // зажигаем светодиод FLAG0
-Delay (500000);
-__builtin_sysreg_write(__FLAGREGCL, 0x00); // гасим светодиод FLAG0
-__builtin_sysreg_write(__FLAGREGST, 0x02); // зажигаем светодиод FLAG1
-Delay (500000);
-__builtin_sysreg_write(__FLAGREGCL, 0x00);
-__builtin_sysreg_write(__FLAGREGST, 0x04); // зажигаем светодиод FLAG2
-Delay (500000);
-__builtin_sysreg_write(__FLAGREGCL, 0x00);
-__builtin_sysreg_write(__FLAGREGST, 0x08); // зажигаем светодиод FLAG3
-Delay (500000);
-__builtin_sysreg_write(__FLAGREGCL, 0x00);
+         HAL_SYS_FlagEnable(); // Основной цикл
+         while(1) {
+                  __builtin_sysreg_write(__FLAGREGST, 0x01); // зажигаем светодиод FLAG0
+                  Delay (500000);
+                  __builtin_sysreg_write(__FLAGREGCL, 0x00); // гасим светодиод FLAG0
+                  __builtin_sysreg_write(__FLAGREGST, 0x02); // зажигаем светодиод FLAG1
+                  Delay (500000);
+                  __builtin_sysreg_write(__FLAGREGCL, 0x00);
+                  __builtin_sysreg_write(__FLAGREGST, 0x04); // зажигаем светодиод FLAG2
+                  Delay (500000);
+                  __builtin_sysreg_write(__FLAGREGCL, 0x00);
+                  __builtin_sysreg_write(__FLAGREGST, 0x08); // зажигаем светодиод FLAG3
+                  Delay (500000);
+                  __builtin_sysreg_write(__FLAGREGCL, 0x00);
          }
 }
 //Функция задержки
